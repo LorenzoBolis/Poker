a694122d7ee79551924d972a78a3ca8fb780fbda
LorenzoBolis <bolis.lorenzo.06@itisdalmine.edu.it>
2024-03-11 21:09:42 +0100

    Aggiunta classe Giocatore

---
+++ SERVER_DEFINITIVO/Server_e/Server_e/Giocatore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_e
+{
+    public class Giocatore
+    {
+        private string nome;
+        private List<Carta> mano;
+        private Socket sk;
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public List<Carta> Mano
+        {
+            get { return mano; }
+            set { mano = value; }
+        }
+
+        public Socket Sk
+        {
+            get { return sk; }
+            set { sk = value; }
+        }
+
+        public Giocatore(string n, Socket sk)
+        {
+            Nome = n;
+            Sk = sk;
+        }
+    }
+}
--- SERVER_DEFINITIVO/Server_e/Server_e/obj/Debug/net7.0/Server_e.AssemblyInfo.cs
+++ SERVER_DEFINITIVO/Server_e/Server_e/obj/Debug/net7.0/Server_e.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server_e")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b1729cd350036d9a27c2d42239b65da9fb235d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3400daf4717131ef617f77542edfaaab0acc264")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server_e")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server_e")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
